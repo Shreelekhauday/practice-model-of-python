718258516d9066845a7650f06603744e91ec4faf
Shreelekhauday <167525951+Shreelekhauday@users.noreply.github.com>
2024-04-28 11:15:13 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,6 @@
+if __name__ == '_main_':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
---
+++ task3.py
@@ -0,0 +1,7 @@
+if __name__ == '_main_':
+    a = int(input())
+    b = int(input())
+    #Integer/floor division ->integer part
+    #Float division-> integer part with decimal
+print(a//b)
+print(a/b)
